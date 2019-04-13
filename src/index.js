cb368c13cd4d7a6d9925483308aafe4f304b0a34
github username <patcarrasco@outlook.com>
2019-04-13 13:10:59 -0400

    added provider/store to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 
+import {Provider} from 'react-redux'
+import configureStore from './store/configureStore'
+
 import App from './components/App.jsx';
+
+const store = configureStore()
 
 // Require Sass file so webpack can build it
 import bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import style from './styles/style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
